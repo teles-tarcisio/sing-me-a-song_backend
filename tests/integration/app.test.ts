4485d65cf9828c84bb7d57777ba95235cf2b93c1
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2022-05-06 12:57:03 -0300

    test: reconfiguring eslint, added basic insertion error to validate

--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -1,20 +1,27 @@
 import supertest from 'supertest';
 import app from '../../src/app.js';
+import { prisma } from '../../src/database.js';
 
-// beforeEach()
+beforeEach(async () => {
+  await prisma.$executeRaw`TRUNCATE TABLE recommendations;`;
+});
 
-// afterEach()
+afterAll(async () => {
+  await prisma.$disconnect();
+});
 
 describe('POST /recommendations', () => {
   it('should successfully send a recommendation given a valid body', async () => {
-    const newMusic =  {
-      name: "Falamansa - Xote dos Milagres",
-      youtubeLink: "https://www.youtube.com/watch?v=chwyjJbcs1Y"
+    const newMusic = {
+      name: 'Falamansa - Xote dos Milagres',
+      youtubeLink: 'https://www.youtube.com/watch?v=chwyjJbcs1Y',
     };
+    // Casuarina - Ponto de Vista, https://www.youtube.com/watch?v=1dmQmMUdMt8
 
     const result = await supertest(app).post('/recommendations').send(newMusic);
     expect(result.status).toEqual(201);
 
-    // const duplicated = await supertest(app).post('/recommendations').send(newMusic);
+    const duplicated = await supertest(app).post('/recommendations').send(newMusic);
+    expect(duplicated.status).toEqual(500);
   });
 });
