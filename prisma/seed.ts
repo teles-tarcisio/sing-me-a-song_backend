13737b5d5e96cd8091f0734d9a0c3b26ae334709
Tarcísio F. S. Teles <tfst@ecomp.poli.br>
2022-05-08 20:46:21 -0300

    feat: added test 'upvote recommendation'

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,17 @@
 import { prisma } from '../src/database.js';
 
 async function main() {
-	//upsert = update/insert
-	//melhor que create por que pode dar conflito em campos unicos
-
-
   const newMusic1 = {
     name: 'Casuarina - Ponto de Vista',
     youtubeLink: 'https://www.youtube.com/watch?v=1dmQmMUdMt8',
   };
-  
+
   const newMusic2 = {
     name: 'Falamansa - Xote dos Milagres',
-    youtubeLink:'https://www.youtube.com/watch?v=chwyjJbcs1Y',
+    youtubeLink: 'https://www.youtube.com/watch?v=chwyjJbcs1Y',
   };
 
-
-	await prisma.recommendation.upsert({
+  await prisma.recommendation.upsert({
     where: { name: newMusic1.name },
     update: {},
     create: {
--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -1,9 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import supertest from 'supertest';
 import app from '../../src/app.js';
 import { prisma } from '../../src/database.js';
 
-
-beforeEach(async () => {
+beforeAll(async () => {
   await prisma.$executeRaw`TRUNCATE TABLE recommendations RESTART IDENTITY CASCADE;`;
 });
 
@@ -11,29 +11,43 @@ afterAll(async () => {
   await prisma.$disconnect();
 });
 
+const newMusic = {
+  name: 'Casuarina - Ponto de Vista',
+  youtubeLink: 'https://www.youtube.com/watch?v=1dmQmMUdMt8',
+};
+
 describe('POST /recommendations', () => {
   it('should successfully insert a recommendation given a valid body', async () => {
-    const newMusic = {
-      name: 'Casuarina - Ponto de Vista',
-      youtubeLink: 'https://www.youtube.com/watch?v=1dmQmMUdMt8',
-    };
-    
     const result = await supertest(app).post('/recommendations').send(newMusic);
     const addedMusic = await prisma.recommendation.findUnique({
-      where: { name: newMusic.name }
+      where: { name: newMusic.name },
     });
     console.log(addedMusic);
-    
 
     expect(result.status).toEqual(201);
-    expect(addedMusic.score).toEqual(0);
-    // tentar com expect.objectContaining(object)
-    
-
-    /*
-    const duplicated = await supertest(app).post('/recommendations').send(newMusic);
-    expect(duplicated.status).toEqual(500);
-    */
-
+    expect(addedMusic).toMatchObject({
+      name: newMusic.name,
+      youtubeLink: newMusic.youtubeLink,
+      score: 0,
+    });
   });
+
+  it('should successfully upvote a video', async () => {
+    const { id, score } = await prisma.recommendation.findUnique({
+      where: { name: newMusic.name },
+    });
+
+    const result = await supertest(app).post(`/recommendations/${id}/upvote`);
+    expect(result.status).toEqual(200);
+    const lastUpvoted = await prisma.recommendation.findUnique({
+      where: { name: newMusic.name },
+    });
+    expect(lastUpvoted.score).toEqual(score + 1);
+  });
+
+  it.todo('downvote a recommendation');
+  it.todo('get last 10 recommendations');
+  it.todo('get recommendation by id');
+  it.todo('get X recommendations sorted by score');
+  it.todo('get recommendation randomly');
 });
